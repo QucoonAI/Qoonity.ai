d58baded7ea1fa5cae7da2e3522c816d7945207c
FelixFrankFelix <frankozie4life@gmail.com>
2025-03-14 11:49:22 +0100

    added env

--- main.py
+++ main.py
@@ -4,7 +4,13 @@ from qooneous import qoonity_head
 from botocore.exceptions import ClientError
 
 session = boto3.Session()
-bedrock = session.client(service_name='bedrock-runtime', region_name="us-east-1")
+
+
+aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
+aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+region_name = os.getenv("AWS_REGION")
+
+bedrock = session.client(service_name='bedrock-runtime', region_name="us-east-1", aws_access_key_id=aws_access_key_id, aws_secret_access_key=aws_secret_access_key)
 modelId = "anthropic.claude-3-5-sonnet-20240620-v1:0"
 memory_list = []
 
